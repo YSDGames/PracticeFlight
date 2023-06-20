68e2a68e78acbe69195e5e32e1e76915f1451e85
YSDGames <135222981+YSDGames@users.noreply.github.com>
2023-06-20 19:53:28 +0900

    0620

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] List<Sprite> center;
+    [SerializeField] List<Sprite> right;
+    [SerializeField] List<Sprite> left;
+    [SerializeField] PlayerBullet bullet;
+    [SerializeField] Transform parent;
+    [SerializeField] Transform bulletParent;
+
+
+    public float fireDelayTime;
+    public float fireTimer;
+
+
+
+    enum Direction
+    {
+        center,
+        left,
+        right
+    }
+
+    Direction dir = Direction.center;
+
+    public float speed;
+
+    void Awake()
+    {
+        GetComponent<SpriteAnimation>().SetSprite(center, 0.2f);
+        bullet.speed = 2f;
+    }
+
+
+
+    void Update()
+    {
+        Move();
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        fireTimer += Time.deltaTime;
+        if (fireTimer > fireDelayTime)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                PlayerBullet b = Instantiate(bullet, parent);
+                b.transform.SetParent(bulletParent);
+                fireTimer = 0;
+            }
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        float y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+
+        if (x == 0 && dir != Direction.center)
+        {
+            dir = Direction.center;
+            GetComponent<SpriteAnimation>().SetSprite(center, 0.2f);
+        }
+        else if (x > 0 && dir != Direction.right)
+        {
+            dir = Direction.right;
+            GetComponent<SpriteAnimation>().SetSprite(right, 0.2f);
+
+        }
+        else if (x < 0 && dir != Direction.right)
+        {
+            dir = Direction.left;
+            GetComponent<SpriteAnimation>().SetSprite(left, 0.2f);
+
+        }
+
+        float clampX = Mathf.Clamp(transform.position.x + x, -2.5f, 2.5f);
+        float clampY = Mathf.Clamp(transform.position.y + y, -4.5f, 4.5f);
+
+        transform.position = new Vector3(clampX, clampY, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [HideInInspector] public float speed; 
+   
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+}
